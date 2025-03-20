d09cd1c70527b48995e1d4c219cb9b771d797c60
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-20 17:16:34 -0300

    add bcryptjs for password hashing in CreateAccountController

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,5 +1,11 @@
-import { ConflictException } from '@nestjs/common'
-import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('/accounts')
@@ -9,23 +15,25 @@ export class CreateAccountController {
   @Post()
   @HttpCode(201)
   async handle(@Body() body: any) {
-    const { name, email, password} = body
+    const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
-        email
-      }
+        email,
+      },
     })
 
     if (userWithSameEmail) {
       throw new ConflictException('Email already in use')
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password
+        password: hashedPassword,
       },
     })
   }
