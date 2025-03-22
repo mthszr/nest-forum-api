1c993be256434b367a3a8aba582f7af5bacea8d9
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-22 10:52:03 -0300

    implement fetch recent questions endpoint and enhance question creation with Zod validation

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,15 +1,56 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodyshcema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user.sub)
+  constructor(private prisma: PrismaService) {}
 
-    return 'ok'
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe)
+    body: CreateQuestionBodyshcema,
+    @CurrentUser() user: UserPayload
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return (
+      title
+        .toLowerCase()
+        .normalize('NFD')
+        // biome-ignore lint/suspicious/noMisleadingCharacterClass: <explanation>
+        .replace(/[\u0300-\u036f]/g, '') // Remove diacritics
+        .replace(/[^\w\s-]/g, '') // Remove non-word chars
+        .replace(/\s+/g, '-') // Replace spaces with hyphens
+        .replace(/^-+|-+$/g, '') // Trim hyphens
+    )
   }
 }
